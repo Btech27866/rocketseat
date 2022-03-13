e8f988005c686bd0d01e3046c1edb911a04cbcaa
Bruno A Amaral <beagle.git27866@gmail.com>
2022-03-13 14:15:36 -0400

    Correção de bugs

--- src/pages/_documents.tsx
+++ src/pages/_documents.tsx
@@ -8,6 +8,7 @@ export default class MyDocument extends Document {
                     <title>ignews</title>
                 </Head>
                     <link rel="preconnect" href="https://fonts.googleapis.com"/>
+                    <link rel="shortcut icon" href="favicon.png" type="images/png" />
                     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap" rel="stylesheet"/>
                 <body>
                     <Main/>
