4a509fa86539ceadbe5eb31d0a96bb6298d9537d
Bruno A Amaral <beagle.git27866@gmail.com>
2022-03-15 17:18:20 -0400

    autenticação pelo github configurada

--- src/components/SigInButton/index.tsx
+++ src/components/SigInButton/index.tsx
@@ -1,23 +1,28 @@
 import {FaGithub} from 'react-icons/fa'
 import {FiX} from 'react-icons/fi'
 import styles from './styles.module.scss'
+import {signIn, signOut,useSession} from 'next-auth/react'
 
 export function SigInButton() {
-    const isUserLogin = true;
+    const {data: session} = useSession()
 
-    return isUserLogin ? (
+    console.log (session)
+
+    return session ? (
         <button
         type="button"
         className={styles.sigInButton}
+        onClick={() => signOut()}
         >
             <FaGithub color="#04D361"/>
-            beagle.git27866
+            {session.user.name}
             <FiX color="#737380" className={styles.closeIcon}/>            
         </button>
     ):(
         <button
         type="button"
         className={styles.sigInButton}
+        onClick={() => signIn('github')}
         >
             <FaGithub color="#EBA417"/>
             Sig in with Github          
