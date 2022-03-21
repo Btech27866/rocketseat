c6acb1083914fc0bcb3c8c1db2636cb8ad15a2bd
Bruno A Amaral <beagle.git27866@gmail.com>
2022-03-21 17:05:47 -0400

    test criação de usuário

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
-import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import { query as q } from 'faunadb'
+import NextAuth from 'next-auth'
+import GithubProvider from 'next-auth/providers/github'
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   providers: [
@@ -12,5 +14,22 @@ export default NextAuth({
         }
       }
     }),
-  ],
+  ],  
+
+  callbacks: {
+    async signIn({ user }) {
+      const { email } = user
+      try {
+        await fauna.query(
+          q.Create(
+            q.Collection('users'),
+            { data: { email } }
+          )
+        )
+        return true
+      } catch {
+        return false
+      }
+    },
+  }
 })
